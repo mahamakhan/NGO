5186862c60d3e110c24d4f7d8cc098bb149becf9
Maham Khan <mahamimranamjad@gmail.com>
2022-09-14 08:48:29 -0700

    added v-if for locations

--- ngoapp/src/components/FooterComp.vue
+++ ngoapp/src/components/FooterComp.vue
@@ -1,5 +1,5 @@
 <template>
-<footer class="text-center lg:text-left bg-gray-100 text-gray-600 -mt-2">
+<footer class="text-center lg:text-left bg-gray-100 text-gray-600 ">
   <div class="flex justify-center items-center lg:justify-between p-6 border-b border-gray-300">
     <div class="mr-12 hidden lg:block">
       <span>Get connected with us on social networks:</span>
--- ngoapp/src/components/HomePage.vue
+++ ngoapp/src/components/HomePage.vue
@@ -10,9 +10,13 @@ The World Association of Non-Governmental Organizations is a global organization
 
         </h4>
       </div>
-      <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-white-300 font-bold">Here is a list of locations to donate food:</h1>
-      <div v-for="list in list" :key="list.id" class=" inline-block mx-10 py-12">
+      
+      <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-white-300 font-bold" >Here is a list of locations to donate food:</h1>
+      <button @click="showlocations()">Show</button>
+      <div v-if="locations">
+      <div v-for="list in list" :key="list.id" class=" inline-block mx-10 py-12" >
         <h1>{{list.name}}</h1>
+      </div>
       </div>
     </div>
     
@@ -24,7 +28,7 @@ The World Association of Non-Governmental Organizations is a global organization
       name: 'HomePage',
       data: () => ({
         list: [],
-        
+        locations: false
          
         
     }),
@@ -36,6 +40,10 @@ The World Association of Non-Governmental Organizations is a global organization
             const res = await axios.get('https://www.givefood.org.uk/api/2/locations/');
             this.list = res.data;
             console.log(res.data + "getlist");
+        },
+        showlocations(){
+          this.locations=!this.locations
+          console.log(this.locations)
         }
     }}
   </script>

97a1d75b470669e013b0d3a05c4a91d1fac72d4a
Maham Khan <mahamimranamjad@gmail.com>
2022-09-13 22:10:12 -0700

    frontend deploy on heroku

--- ngoapp/src/globals.js
+++ ngoapp/src/globals.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'http://127.0.0.1:8000'
-const BASE_URL= 'https://mahamngobackend.herokuapp.com/'
+const BASE_URL= 'https://mahamngobackend.herokuapp.com'
 
 module.exports = {
   BASE_URL,
